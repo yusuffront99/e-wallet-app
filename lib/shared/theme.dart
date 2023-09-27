161db33d7652eeeb5fd8a7b96f58b117d5dcc7be
unknown <you@example.com>
2023-09-28 05:52:30 +0800

    updated and create a new page --onboarding_page with carousel slider

--- lib/shared/theme.dart
+++ lib/shared/theme.dart
@@ -4,20 +4,35 @@ import 'package:google_fonts/google_fonts.dart';
 Color whiteColor = const Color(0xffFFFFFF);
 Color blackColor = const Color(0xff14193F);
 Color greyColor = const Color(0xffA4A8AE);
+Color lightGreyColor = const Color(0xffF1F1F9);
 Color lightBackgroundColor = const Color(0xffF6F8FB);
 Color darkBackgroundColor = const Color(0xff020518);
 Color blueColor = const Color(0xff53C1F9);
 Color purpleColor = const Color(0xff5142E6);
 Color greenColor = const Color(0xff22B07D);
 Color numberBackgroundColor = const Color(0xff1A1D2E);
+Color redColor = const Color(0xffFF2566);
 
-TextStyle blackTextStyle = GoogleFonts.poppins(color: blackColor);
-TextStyle whiteTextStyle = GoogleFonts.poppins(color: whiteColor);
-TextStyle greyTextStyle = GoogleFonts.poppins(color: greyColor);
-TextStyle blueTextStyle = GoogleFonts.poppins(color: blueColor);
-TextStyle greenTextStyle = GoogleFonts.poppins(color: greenColor);
+TextStyle blackTextStyle = GoogleFonts.poppins(
+  color: blackColor,
+);
 
-// === shardness
+TextStyle whiteTextStyle = GoogleFonts.poppins(
+  color: whiteColor,
+);
+
+TextStyle greyTextStyle = GoogleFonts.poppins(
+  color: greyColor,
+);
+
+TextStyle blueTextStyle = GoogleFonts.poppins(
+  color: blueColor,
+);
+
+TextStyle greenTextStyle = GoogleFonts.poppins(
+  color: greenColor,
+);
+
 FontWeight light = FontWeight.w300;
 FontWeight regular = FontWeight.w400;
 FontWeight medium = FontWeight.w500;
--- lib/ui/pages/onboarding_page.dart
+++ lib/ui/pages/onboarding_page.dart
@@ -1,10 +1,168 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:e_wallet_app/shared/theme.dart';
 import 'package:flutter/material.dart';
 
-class OnboardingPage extends StatelessWidget {
+class OnboardingPage extends StatefulWidget {
   const OnboardingPage({super.key});
 
   @override
+  State<OnboardingPage> createState() => _OnboardingPageState();
+}
+
+class _OnboardingPageState extends State<OnboardingPage> {
+  int currentIndex = 0;
+  CarouselController carouselController = CarouselController();
+
+  List<String> titles = [
+    'Grow your\n Financial Today',
+    'Build From\n Zero to Freedom',
+    'Start Together',
+  ];
+
+  List<String> subtitles = [
+    'Our system is helping you to\n achive a better goal',
+    'We provide tips for you so that\n you can adapt easier',
+    'We will guide you to where\n you wanted it too',
+  ];
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: lightBackgroundColor,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CarouselSlider(
+              items: [
+                Image.asset(
+                  'assets/img_onboarding1.png',
+                  height: 331,
+                ),
+                Image.asset(
+                  'assets/img_onboarding2.png',
+                  height: 331,
+                ),
+                Image.asset(
+                  'assets/img_onboarding3.png',
+                  height: 331,
+                ),
+              ],
+              options: CarouselOptions(
+                  height: 331,
+                  onPageChanged: (index, reason) {
+                    setState(() {
+                      currentIndex = index;
+                    });
+                  },
+                  viewportFraction: 1, //== agar gambar full disatu halaman
+                  enableInfiniteScroll:
+                      false //== menampilkan gambar slider agar tidak infinity (1-3)
+                  ),
+              carouselController: carouselController,
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 24),
+              padding: const EdgeInsets.symmetric(horizontal: 22, vertical: 24),
+              decoration: BoxDecoration(
+                color: whiteColor,
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Column(
+                children: [
+                  Text(
+                    titles[currentIndex],
+                    style: blackTextStyle.copyWith(
+                      fontSize: 20,
+                      fontWeight: semiBold,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  const SizedBox(
+                    height: 26,
+                  ),
+                  Text(
+                    subtitles[currentIndex],
+                    style: greyTextStyle.copyWith(
+                      fontSize: 16,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  const SizedBox(
+                    height: 50,
+                  ),
+                  Row(
+                    children: [
+                      Container(
+                        width: 12,
+                        height: 12,
+                        margin: const EdgeInsets.only(right: 10),
+                        decoration: BoxDecoration(
+                          // == dot indication ==
+                          shape: BoxShape.circle,
+                          color: currentIndex == 0
+                              ? blueColor
+                              : lightBackgroundColor,
+                        ),
+                      ),
+                      Container(
+                        width: 12,
+                        height: 12,
+                        margin: const EdgeInsets.only(right: 10),
+                        // == dot indication ==
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: currentIndex == 1
+                              ? blueColor
+                              : lightBackgroundColor,
+                        ),
+                      ),
+                      Container(
+                        width: 12,
+                        height: 12,
+                        margin: const EdgeInsets.only(right: 10),
+                        decoration: BoxDecoration(
+                          // == dot indication ==
+                          shape: BoxShape.circle,
+                          color: currentIndex == 2
+                              ? blueColor
+                              : lightBackgroundColor,
+                        ),
+                      ),
+                      const Spacer(),
+                      SizedBox(
+                        width: 150,
+                        height: 50,
+                        child: TextButton(
+                          onPressed: () {
+                            carouselController.nextPage();
+                          },
+                          style: TextButton.styleFrom(
+                            backgroundColor: purpleColor,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(56),
+                            ),
+                          ),
+                          child: Text(
+                            'Continue',
+                            style: whiteTextStyle.copyWith(
+                              fontSize: 16,
+                              fontWeight: semiBold,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
